0e7f26c3d45658462418ebedfe8ab2db19351b54
pk0044 <purushottam0044@gmail.com>
2020-05-08 01:30:04 +0530

    adding leetcode spiral matrix 2 problem

---
+++ src/com/pk/amazon/SpiralMatrix_2.java
@@ -0,0 +1,14 @@
+package com.pk.amazon;
+
+/**
+ * 
+ * problem link : https://leetcode.com/problems/spiral-matrix-ii/
+ * 
+ * @author pk0044
+ *
+ */
+public class SpiralMatrix_2 {
+    public int[][] generateMatrix(int n) {
+        
+    }
+}
