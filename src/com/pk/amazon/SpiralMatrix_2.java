01895fe9ea558163f63f3a0a2b70f8948f7ab1b9
pk0044 <purushottam0044@gmail.com>
2020-05-08 01:51:24 +0530

    adding leetcode unique path problem

--- src/com/pk/amazon/SpiralMatrix_2.java
+++ src/com/pk/amazon/SpiralMatrix_2.java
@@ -9,6 +9,53 @@ package com.pk.amazon;
  */
 public class SpiralMatrix_2 {
     public int[][] generateMatrix(int n) {
+        if(n == 0) {
+        	return new int[0][0];
+        }
         
+        int[][] res = new int[n][n];
+        
+        int count = 1;
+        
+        int rstart = 0;
+        int rend = n - 1;
+        
+        int cstart = 0;
+        int cend = n - 1;
+        
+        while(rstart <= rend && cstart <= cend) {
+        	
+        	for(int i=cstart; i<=cend; i++) {
+        		res[rstart][i] = count;
+        		count++;
+        	}
+        	
+        	for(int i=rstart+1; i<=rend; i++) {
+        		res[i][cend] = count;
+        		count++;
+        	}
+        	
+        	if(rstart < rend) {
+        		for(int i=cend-1; i>=cstart; i--) {
+        			res[rend][i] = count;
+        			count++;
+        		}
+        	}
+        	
+        	if(cstart < cend) {
+        		for(int i=rend-1; i>rstart; i--) {
+        			res[i][cstart] = count;
+        			count++;
+        		}
+        	}
+        	
+        	rstart++;
+        	rend--;
+        	cstart++;
+        	cend--;
+        }
+        
+        return res;
     }
+    
 }
